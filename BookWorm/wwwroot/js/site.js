16f2587be176db6eb6e7bf1176f76ba09e5b55e5
Mohammad Soqar <mnsoqar1@gmail.com>
2023-02-25 23:02:51 +0300

    Add navbar and footer to layout

--- BookWorm/wwwroot/js/site.js
+++ BookWorm/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
 
-// Write your JavaScript code.
+}
