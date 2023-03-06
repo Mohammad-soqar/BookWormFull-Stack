5b170590ab16eb35c10a4baae75ad25461e5faf9
Hamza Amin <90584545+Ham252@users.noreply.github.com>
2023-03-06 18:03:54 +0300

    Added AdminPage HTML&CSS but ngl idk if they work or not cuz I couldn't test them lmao

--- BookWorm/Areas/Customer/Controllers/HomeController.cs
+++ BookWorm/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookWorm.Controllers
+namespace BookWorm.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
